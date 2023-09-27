448b132d8349c78e366d89421b6b70f28da3e409
Abdallah Massri <a.v.e@live.com>
2023-09-27 03:57:16 +0300

    fixing the variable and the output again to get the except result

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -1,11 +1,15 @@
-print("Hello, whats your name?")
 name = "Michael"
-print(name)
-print(name + ", please choose board size:")
 board_size = 9
-print(board_size)
-print(name + ", for board size " + str(board_size) + ", choose number of mines to allocate:")
 number_of_mines = 7
+
+print("Hello, whats your name?")
+print(name)
+
+print(name + ", please choose board size:")
+print(board_size)
+
+print(name + ", for board size " + str(board_size) + ", choose number of mines to allocate:")
 print(number_of_mines)
+
 print((name + ", the board size is: "+str(board_size)+", number of mines is: "+str(number_of_mines) + ". ENJOY!"))
 
