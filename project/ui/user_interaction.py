b6c4a4ba9f3a4947a948ef8c1a1b911336532b23
Abdallah Massri <a.v.e@live.com>
2023-09-27 10:08:54 +0300

    fixing the variable to get the except result 4

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -2,6 +2,8 @@ name = input("Hello, whats your name?")
 print(name)
 board_size = 9
 print("Michael, please choose board size:")
+print(board_size)
 number_of_mines = 3
 print("Michael, for board size 9, choose number of mines to allocate:")
+print(number_of_mines)
 print("Michael, the board size is: 9, number of mines is: 7. ENJOY!")
