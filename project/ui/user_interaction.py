8f6edaf58b20224022225c041afa941496e70955
Abdallah Massri <a.v.e@live.com>
2023-09-27 03:33:55 +0300

    creating variables from input user

---
+++ project/ui/user_interaction.py
@@ -0,0 +1,8 @@
+name = input("Hello, whats your name?\n")
+print(name)
+board_size = input(name + ", please choose board size:\n")
+print(board_size)
+number_of_mines = input(name + ", for board size " + board_size + ", choose number of mines to allocate:\n")
+print(name + ", the board size is: "+board_size+", number of mines is: "+number_of_mines + ". ENJOY!")
+board_size = int(board_size)
+number_of_mines = int(number_of_mines)
